805b82ce07d339b59c16f05f6a299ce7b124d2a1
Shahob Jumaev <69839721+shaxxxob@users.noreply.github.com>
2022-07-16 21:05:21 +0500

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.gis.admin import OSMGeoAdmin
-from .models import Shop
-
-
-@admin.register(Shop)
-class ShopAdmin(OSMGeoAdmin):
-    list_display = ("name", "location")
