5a800b2006b95abda480f6098a21a2ab8f09f966
Shahob Jumaev <69839721+shaxxxob@users.noreply.github.com>
2022-07-16 20:16:55 +0500

    Create 0001_initial.py

---
+++ nearbyshops/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+import django.contrib.gis.db.models.fields
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Shop',
+            fields=[
+                ('id', models.AutoField(
+                    auto_created=True, primary_key=True, serialize=False,
+                    verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                (
+                    'location',
+                    django.contrib.gis.db.models.fields.PointField(srid=4326)
+                ),
+                ('address', models.CharField(max_length=100)),
+                ('city', models.CharField(max_length=50)),
+            ],
+        ),
+    ]
